3b878803e977177a9eb2c4ec9bfed6e46d765ed3
Brent Lintner <brent.lintner@gmail.com>
2025-10-10 17:52:44 -0400

    The main branch is what we should use, and the new repo name too

--- init.lua
+++ init.lua
@@ -11,8 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
     "git",
     "clone",
     "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "https://github.com/folke/lazy.nvim",
     lazypath,
   })
   vim.cmd [[packadd lazy.nvim]]
